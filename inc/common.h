aaf31c2783c48cb576e7f94e4b9eb87b072e7a49
Benjamin J Anderson <banderson@mashable.com>
2019-01-28 12:39:14 -0600

    set up generic c project w/utils

---
+++ inc/common.h
@@ -0,0 +1,14 @@
+#ifndef __SCANNER_COMMON__
+#define __SCANNER_COMMON__ 1
+
+#include "util/memory.h"
+
+#define SCANNER_MEMORY_DEBUG 1
+#define SCANNER_MEMORY_DEBUG_VERBOSE 0
+#define SCANNER_SCANNER_OUTPUT_VERBOSE 0
+
+typedef enum {
+    FALSE, TRUE,
+} BOOLEAN;
+
+#endif
